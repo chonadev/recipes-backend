a408e4cbf6c5f493495c955d816a6c182d766d52
Jhona <jhonavenegas@gmail.com>
2022-07-28 12:28:59 -0300

    refactor return sum amount ingredients

--- src/main/java/app/ingredient/IngredientDatasource.java
+++ src/main/java/app/ingredient/IngredientDatasource.java
@@ -1,8 +1,15 @@
 package app.ingredient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IngredientDatasource extends JpaRepository<Ingredient, Long> {
+
+
+    @Query(value = "SELECT i.name, SUM(i.amount) FROM Ingredient i WHERE i.name = i.name GROUP BY i.name", nativeQuery = true)
+    List<Object[]> getIngredientsWithOutRepeat();
 }
--- src/main/java/app/ingredient/IngredientServiceImpl.java
+++ src/main/java/app/ingredient/IngredientServiceImpl.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
+import java.util.ArrayList;
+
 public class IngredientServiceImpl implements IngredientService {
 
     private static final Logger logger = LoggerFactory.getLogger(IngredientServiceImpl.class);
@@ -20,7 +22,13 @@ public class IngredientServiceImpl implements IngredientService {
         response.setDescription(HttpStatus.OK.name());
 
         try {
-            response.setIngredients(datasource.findAll());
+            if ( response.getIngredients() == null) response.setIngredients(new ArrayList<>());
+            datasource.getIngredientsWithOutRepeat().forEach( row -> {
+                Ingredient ingredient = new Ingredient();
+                ingredient.setName(row[0].toString());
+                ingredient.setAmount((Double) row[1]);
+                response.getIngredients().add(ingredient);
+            });
         } catch (Exception e){
             logger.error("Error al obtener los ingredientes de la base de datos.");
             response.setCode(HttpStatus.INTERNAL_SERVER_ERROR.toString());
